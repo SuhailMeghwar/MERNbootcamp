6b5e2ecd745d6c9f3a406b6879ed66e54382c30e
Abdul Rafay <54492363+rafay-ah@users.noreply.github.com>
2023-01-29 19:33:05 +0500

    Branch Waashout

--- day2.js
+++
@@ -1,3 +0,0 @@
-////////////////////////////
-let namest="adil";
-console.log(namest);
--- rps.js
+++
@@ -1,9 +0,0 @@
-const prompt = require('prompt-sync')();
-
-const choices = ["scissors", "rock", "paper"];
-
-const userInput = prompt("Lets Play. Enter your choice: ");
-
-let computerChoice = choices[Math.floor(Math.random()*3)];
-
-console.log(`I chose ${computerChoice}`);

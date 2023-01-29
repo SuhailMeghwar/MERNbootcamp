28d211171ca3580719c86aba9fec95938b38ab2b
adilali32819 <123810966+adilali32819@users.noreply.github.com>
2023-01-29 16:41:03 +0500

    day 6 completed

---
+++ first_program.js
@@ -0,0 +1,132 @@
+// console.log("happy learning");
+// let Myname="AdilAli";
+// //keyword+varriable_name=data;
+// //NaN (mathmatical operators m ap kch galat kr rhy) Not A number 2 ttpes apps m compatible
+// //type casting 
+// // concatenation
+// //varriables varriable declare but not assigned properly value define nhi hui                                           
+// Myname=Myname*2;
+// console.log(Myname);
+//udefine data type? 
+//DMAS RULE is use
+// ()  fistevaluate
+// let tempCl=15;
+// let  tempIf=(tempCl*1.8)+32;
+// console.log( "temprature", typeof tempIf);
+// (temperature - 32) * (5/9)
+// let tempIf=15;
+// let  temCl=(tempIf-32)*5/9;
+// console.log( "tep farenhit to celcius",temCl);
+///////////////////////////
+// temprature celcius to farenhit
+// let tempCl=10;
+// let tempIf=((tempCl*9/5)+32);
+// console.log(tempIf);
+// let numer=122334345556666;
+// console.log("number==",numer*99999999);
+// console.log("number==",numer*99999999);
+////////////////////
+// type scripts
+// let  some_number_big=900799254740999n;
+// console.log(  typeof some_number_big);
+// ///////////////////////////
+//  let tempCl=10;
+//  let tempIf=((tempCl*9/5)+32);
+//  console.log(tempIf);
+//  console.log(`The tempratureis:${tempIf}`);
+ /////////////////////
+//  const num1=900799254740999n;
+//  let value=num1*5n;
+//   value=num1/5n;
+// console.log(value);
+// let check=5*"adil";
+// console.log(check);
+// var numberCheck=122345;
+// var val;
+// function sum(n1,n2){
+//     val= n1 + n2;
+    
+// }
+// sum(5,6)
+// console.log(val);
+//  const pi=3.1414;
+//  pi=234223;
+//  console.log(pi);
+//////////////////////////////////
+// install kry gy propmt ko /npm install prompt-sync/ user sy input lane h tu prompt use kry gy
+// const prompt = require('prompt-sync')();
+// const name=prompt('please enter your name');
+// console.log(`hey there is ${name}`);
+
+// let num=1/0;
+// console.log(num);
+// const prompt = require('prompt-sync')();
+//   let tempCl=prompt('please enter a number');
+//  let tempIf=((tempCl*9/5)+32);
+//  console.log(tempIf);
+//  console.log(`The tempratureis:${tempIf}`);
+//  let result=prompt('Please enter result marks')
+//   let pass=result*5;
+//   console.log(`your result is ${pass}`);
+// function sum(n1,n2){
+//      var val= n1 + n2;
+        
+//      }
+//     sum(5,6);
+//     console.log(val
+//         );
+//         for(let i=0; i<5; i++){
+//             var num=0;
+//             var val=num*i;
+//         }
+//         console.log(val
+//             );
+
+//   let tempCl=prompt('please enter a number');
+//  let tempIf=((tempCl*9/5)+32);
+//  console.log(tempIf);
+//  console.log(`The tempratureis:${tempIf}`);
+//  let result=prompt('Please enter result marks')
+//  let result=prompt('')
+//  console.log("typecheck",typeof result);
+//  let pass=result*5;
+//   console.log(`your result is ${pass}`);
+// const prompt = require('prompt-sync')();
+// let n1=prompt('please enter  a first  number');
+
+// let n2=prompt('please enter  a second  number');
+// let result= Number(n1) + Number(n2);
+// console.log(`this is your result ${result}`)
+// const prompt = require('prompt-sync')();
+// let valu1=prompt('please enter  a first  number')
+// let valu2=prompt('please enter  a first  number')
+// //bolean number add int
+// let result= Boolean(valu1) - String(valu2);
+// console.log(result);
+// let n1=5;
+// let n2=4;
+// if(n1>n2){
+//     console.log("n1 is greater than n2");
+// }
+// else console.log("n1 is small than n2");
+// const prompt =require('prompt-sync')();
+
+// let n1=prompt("pls enter first value");
+// let n2=prompt("pls enter Second value");
+// let operator=prompt("select an operatoe a,s,m,d");
+// if(operator=="a"){
+//   console.log (n1+n2);
+// }
+// else if(operator=="s"){
+//   console.log(n1-n2);
+// }
+// else if(operator=="m"){
+//   console.log (n1*n2);
+// }
+// else if(operator=="d");
+// {
+//   console.log (n1/n2);
+// }
+ 
+// let cel=prompt()
+

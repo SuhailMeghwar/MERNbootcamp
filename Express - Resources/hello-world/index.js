602471c082ada8939ae983cfdc4adf172fe62250
Abdul Rafay <54492363+rafay-ah@users.noreply.github.com>
2023-04-17 16:22:12 +0500

    mongo db +blog app

--- Express - Resources/hello-world/index.js
+++ Express - Resources/hello-world/index.js
@@ -3,6 +3,11 @@ const app = express();
 
 let messages = []
 
+// middlewares 
+// app.use()
+
+app.use(express.json());
+
 //Declare routes
 
 app.get("/", (req, res, next) => {
@@ -10,7 +15,8 @@ app.get("/", (req, res, next) => {
 });
 
 app.get("/message",(req, res)=>{
-  messages.push(req.query.text)
+  messages.push(req.query.id)
+  // request.body.text
   res.send("Your message has been recieved!")
 })
 
@@ -18,6 +24,12 @@ app.get("/getAllmessage",(req,res)=>{
 res.send(messages)
 })
 
+
+app.post("/insert-message/:profile_id",(req,res)=>{
+  req.params.profile_id
+  console.log(req.body)
+  res.send({"log": "message recieved"})
+  })
 
 
 app.listen(5000, () => {
